5d36d3de50cb095236cc8f6bf8065d43f5f77c06
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-06-19 18:44:10 +0530

    filesetup

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,22 +1,56 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+/*import { Route, Router } from 'react-router';*/
 import { Layout } from './components/Layout';
-import {Home}  from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-
+import { HeadLayout } from './components/HeadLayout';
+import Home  from './components/HomePage/Home';
+import { NavMenu } from './components/HomePage/NavMenu';
+/*import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';*/
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './custom.css'
-
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
+import { UserPage } from './components/UserPage/UserPage';
+import CoursesPage from './components/Courses/CoursesPage';
+import EventPage from './components/Event/EventPage';
+import QuizPage from './components/Quiz/QuizPage';
+import Signup from './components/AuthPage/Signup';
+import Login from './components/AuthPage/Login';
+function App() {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+        <Router>
+            <div className="app">
+                <Switch>
+                    <Router exact path="/">
+                        <Layout>
+                            <Route exact path='/' component={Home}/>
+                            <Route path='/auth/signup' component={Signup} />
+                            <Route path='/auth/login' component={Login} />
+                        </Layout>
+                     
+                    </Router>
+                    <Router path="/auth">
+                      
+                    </Router>
+                <Router path="/userpage">
+                        <HeadLayout>
+                            <Route exact path='/userpage' component={UserPage} />
+                            <Route path='/userpage/course' component={CoursesPage} />
+                            <Route path='/userpage/event' component={EventPage} />
+                            <Route path='/userpage/quiz' component={ QuizPage} />
+                        </HeadLayout>
+                  
+                    </Router>
+                    {/* <Router path="/courses">
+                        <HeadLayout>
+                            <Route path='/course' component={CoursesPage} />
+                            <Route path='/event' component={EventPage} />
+                            <Route path='/quiz' component={QuizPage} />
+                        </HeadLayout>
+                        <CoursesPage />
+                    </Router>*/}
+                </Switch>
+            </div>
+        </Router>
+    )
 }
+
+export default App;
---
+++ ClientApp/src/components/Quiz/QuizPage.js
@@ -0,0 +1,6 @@
+﻿import React from 'react';
+const QuizPage = () => {
+    return (
+        <><h1>Quiz page</h1></>)
+}
+export default QuizPage;
