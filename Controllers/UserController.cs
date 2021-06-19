a8940d5e9bb094d65ef0e5daae6c2e36131e5e30
Maaz Bin Asad <60134184+maaz-bin-asad@users.noreply.github.com>
2021-06-20 01:55:11 +0530

    Add register facility

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,6 +5,9 @@ using React5.Models;
 using System;
 using React5.Database;
 using System.Data.SQLite;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text;
 
 
 namespace React5.Controllers
@@ -19,18 +22,36 @@ namespace React5.Controllers
         public UserController()
         {
         }
-        [HttpPost]
+        static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+            [HttpPost]
         public RedirectResult Insert([FromForm] User user)
 
         {
             string mail = Convert.ToString(user.mail);
             string pass = Convert.ToString(user.hashpassword);
-            Console.WriteLine(mail);  //debugging
-            Console.WriteLine(pass);  //debugging
+            string hashed = ComputeSha256Hash(pass);
+            Console.WriteLine(hashed);
             con.OpenConnection();
             string query = "SELECT * FROM user where hashpassword=@pass";
             SQLiteCommand myCommand = new SQLiteCommand(query, con.myConnection);
-            myCommand.Parameters.AddWithValue("@pass", pass);
+            myCommand.Parameters.AddWithValue("@pass", hashed);
             SQLiteDataReader result = myCommand.ExecuteReader();
             if (result.HasRows)
             {
@@ -61,6 +82,42 @@ namespace React5.Controllers
                 con.CloseConnetion();
                 return Redirect("/");
             }
+
+        }
+        [HttpPost("{signup}")]
+
+        public RedirectResult Register([FromForm] User user)
+        {
+            string mail = Convert.ToString(user.mail);
+            string pass = Convert.ToString(user.hashpassword);
+            string hashed = ComputeSha256Hash(pass);
+            string username = Convert.ToString(user.username);
+            con.OpenConnection();
+            string query = "SELECT * FROM user where mail=@email";
+            SQLiteCommand myCommand = new SQLiteCommand(query, con.myConnection);
+            myCommand.Parameters.AddWithValue("@email", mail);
+            SQLiteDataReader result = myCommand.ExecuteReader();
+            if (result.HasRows)
+            {
+                con.CloseConnetion();
+                return Redirect("/auth/signup");
+
+            }
+            else
+            {
+                con.CloseConnetion();
+                con.OpenConnection();
+                query = "insert into user ('username', 'hashpassword', 'mail') values(@username, @hashpassword, @mail)";
+                SQLiteCommand comm = new SQLiteCommand(query, con.myConnection);
+                comm.Parameters.AddWithValue("@username", username);
+                comm.Parameters.AddWithValue("@hashpassword", hashed);
+                comm.Parameters.AddWithValue("@mail", mail);
+                var res = comm.ExecuteNonQuery();
+                con.CloseConnetion();
+                return Redirect("/auth/login");
+            }
+
+
 
         }
 
