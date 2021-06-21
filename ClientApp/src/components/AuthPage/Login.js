4fe9a04f3f39e2ff580e102cad05f57dde45de10
Jatin204s <Jatin204s@gmail.com>
2021-06-21 18:42:03 +0530

    first commit

--- ClientApp/src/components/AuthPage/Login.js
+++ ClientApp/src/components/AuthPage/Login.js
@@ -1,16 +1,57 @@
 ﻿import React from 'react';
+import './Auth.css'
 
 const Login = () => {
     return (
-        <><div>
-            <h1>Hello, Learner!</h1>
-            <p>Welcome to Perspectify</p>
-
-            <form action="User" method="post">
-                <input type="text" name="mail" placeholder="Email"></input>
-                <input type="password" name="hashpassword" placeholder="Password"></input>
-                <input type="submit" value="Login"></input>
-            </form>
-        </div></>)
+			 <>
+			<div className="container1">
+				<div className="d-flex justify-content-center h-100">
+					<div className="card">
+						<div className="card-header">
+							<h3>Sign In</h3>
+							<div className="d-flex justify-content-end social_icon">
+								<span><i className="fab fa-facebook-square"></i></span>
+								<span><i className="fab fa-google-plus-square"></i></span>
+								<span><i className="fab fa-twitter-square"></i></span>
+							</div>
+						</div>
+						<div className="card-body">
+							<form action="User" method= "post">
+								<div className="input-group form-group">
+									<div className="input-group-prepend">
+										<span className="input-group-text"><i className="fas fa-envelope"></i></span>
+									</div>
+									<input type="text" className="form-control" placeholder="Email id" name="mail"/>
+						
+								</div>
+								<div className="input-group form-group">
+										<div className="input-group-prepend">
+											<span className="input-group-text"><i class="fas fa-key"></i></span>
+									</div>
+									<input type="password" className="form-control" placeholder="password" name="hashpassword"/>
+								</div>
+										<div className="row align-items-center remember"/>
+											
+									
+											<div className="form-group">
+												<input type="submit" value="Login" className="btn float-right login_btn"/>
+									</div>
+							</form>
+										</div>
+										<div className="card-footer">
+											<div className="d-flex justify-content-center links">
+												Don't have an account?<a href="#">Sign Up</a>
+											</div>
+											<div className="d-flex justify-content-center">
+												<a href="#">Forgot your password?</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							
+          
+		  </>
+    )
 }
 export default Login;
