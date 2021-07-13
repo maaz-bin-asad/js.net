b52766080917c922726a49dd48d16e955e471543
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-07-13 11:45:08 +0530

    remove protected route exception

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -42,10 +42,10 @@ function App() {
                      
                         <Router path="/userpage">
                             <HeadLayout>
-                            <ProtectedRoute exact path='/userpage' component={UserPage} />
-                                <ProtectedRoute path='/userpage/course' component={CoursesPage} />
-                                <ProtectedRoute path='/userpage/event' component={EventPage} />
-                                <ProtectedRoute path='/userpage/quiz' component={QuizPage} />
+                            <Route exact path='/userpage' component={UserPage} />
+                                <Route path='/userpage/course' component={CoursesPage} />
+                                <Route path='/userpage/event' component={EventPage} />
+                                <Route path='/userpage/quiz' component={QuizPage} />
                             </HeadLayout>
 
                         </Router>
