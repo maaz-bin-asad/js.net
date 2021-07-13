6f4d88b82ded9ae4549fd1542d4db77e5b4f4014
Maaz Bin Asad <60134184+maaz-bin-asad@users.noreply.github.com>
2021-07-13 12:59:50 +0530

    Update bg image

--- ClientApp/src/components/HomePage/Home.js
+++ ClientApp/src/components/HomePage/Home.js
@@ -2,25 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
-
-/*export class Home extends Component {
-    static displayName = Home.name;
-
-    render() {
-        return (
-            <div>
-                <h1>Hello, Learner!</h1>
-                <p>Welcome to Perspectify</p>
-
-                <form action="User" method="post">
-                    <input type="text" name="mail" placeholder="Email"></input>
-                    <input type="password" name="hashpassword" placeholder="Password"></input>
-                    <input type="submit" value="Login"></input>
-                </form>
-            </div>
-        );
-    }
-}*/
 const Home = () => {
     return (<>
         <div className="home">
--- Services/CourseServices.cs
+++ Services/CourseServices.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.SQLite;
 using React5.Database;
 using React5.Models;
