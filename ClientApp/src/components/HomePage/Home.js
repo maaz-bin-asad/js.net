9907aa07ed3662b75e4b2c036be43500eded09cf
Maaz Bin Asad <60134184+maaz-bin-asad@users.noreply.github.com>
2021-07-14 18:55:58 +0530

    Update Home.js

--- ClientApp/src/components/HomePage/Home.js
+++ ClientApp/src/components/HomePage/Home.js
@@ -10,16 +10,6 @@ const Home = () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 <Link tag={Link} className="btn1" to="/auth/signup">Get started</Link>
             </div>
-            {/*<div>
-                <h1>Hello, Learner!</h1>
-                <p>Welcome to Perspectify</p>
-
-                <form action="User" method="post">
-                    <input type="text" name="mail" placeholder="Email"></input>
-                    <input type="password" name="hashpassword" placeholder="Password"></input>
-                    <input type="submit" value="Login"></input>
-                </form>
-            </div>*/}
         </div>
        
     </>
