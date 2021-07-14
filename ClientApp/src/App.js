a033c8d685ec81221ef22fe95f2609b910c979d1
Maaz Bin Asad <60134184+maaz-bin-asad@users.noreply.github.com>
2021-07-14 19:32:13 +0530

    Update App.js

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-/*import { Route, Router } from 'react-router';*/
 import { Layout } from './components/Layout';
 import { HeadLayout } from './components/HeadLayout';
 import Home  from './components/HomePage/Home';
 import { NavMenu } from './components/HomePage/NavMenu';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './custom.css'
-
 import UserPage from './components/UserPage/UserPage';
 import CoursesPage  from './components/Courses/CoursesPage';
-
 import EventPage from './components/Event/EventPage';
 import { QuizPage } from './components/Quiz/QuizPage';
 import Signup from './components/AuthPage/Signup';
@@ -50,16 +47,6 @@ function App() {
 
                         </Router>
 
-                  {/*  
-                    <Router path="/userpage/course">
-                        <SideBarLayout>
-                            <Route exact path='/userpage/course' component={Courses} />
-                            <Route path='/userpage/course/frontend' component={FrontEndCourse} />
-                            <Route path='/userpage/course/backend' component={BackEndCourse} />
-                            <Route path='/userpage/course/visual' component={VisualCourse} />
-                        </SideBarLayout>
-                            </Router>
-                    */}
                 </Switch>
             </div>
         </Router>
